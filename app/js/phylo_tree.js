554a11fc62c1ffe62beaa896068db759153ffe4e
AntoTr4 <antonin.leyval@ecl16.ec-lyon.fr>
2020-02-15 18:24:32 +0100

    Refactorization of app

--- app/js/phylo_tree.js
+++ app/js/phylo_tree.js
@@ -1,78 +1,30 @@
-<!DOCTYPE html>
-<head>
-  <meta charset="utf-8">
-  <script src="https://d3js.org/d3.v4.min.js"></script>
-  <script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
-  <title>ENDSPE</title>
-  <style>
-    body { margin:0;position:fixed;top:0;right:0;bottom:0;left:0; }
-    
-    .node circle {
-      fill: #999;
-    }
-    
-    .node text {
-      font: 10px sans-serif;
-    }
-    
-    .node--internal circle {
-      fill: #555;
-    }
-    
-    .node--internal text {
-      text-shadow: 0 1px 0 #fff, 0 -1px 0 #fff, 1px 0 0 #fff, -1px 0 0 #fff;
-    }
-    
-    .link {
-      fill: none;
-      stroke: #555;
-      stroke-opacity: 0.4;
-      stroke-width: 1.5px;
-    }
-    
-  </style>
-</head>
-
-<body>
-  <svg width="960" height="900"></svg>
-  <script>
-    // Feel free to change or delete any of the code you see in this editor!
-    //  var svg = d3.select("body").append("svg")
-    //    .attr("width", 960)
-    //    .attr("height", 500)
-
-    //  var margin = {top: 60, right: 60, bottom: 60, left: 60};
-
-    //  var width = 960 - margin.left - margin.right,
-    //      height = 500 - margin.top - margin.bottom;
-    //          
-    //  var padding = {h: 15, v: 15}
-    
+// Display a phylo tree with datas from url
+function phyloTree(url) {
     var svg = d3.select("svg"),
         width = +svg.attr("width"),
         height = +svg.attr("height"),
         g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + (height / 2 - 30) + ")");
-    
+
     var stratify = d3.stratify()
         .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
-    
+
     var cluster = d3.cluster()
         .size([360, width / 2 - 200]);
-    
+
     let color = d3.scaleSequential(t => {
                         if (t < 0.75) { return d3.interpolateInferno(t) }
                         else { return d3.interpolateViridis(1 - t + 0.75) }
               })
               .domain([-3, 3]);
 
-    d3.csv("https://raw.githubusercontent.com/ddonatien/endspe/master/phylo_animal.csv", function(error, data) {
+    d3.csv(url, function(error, data) {
       if (error) console.log(error);
-    
+
       var root = stratify(data)
           .sort(function(a, b) { return a.height - b.height || a.id.localeCompare(b.id); });
-    
+
       cluster(root);
-    
+
       var link = g.selectAll(".link")
           .data(root.descendants().slice(1))
           .enter().append("path")
@@ -83,32 +35,31 @@
                 + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
                 + " " + project(d.parent.x, d.parent.y);
           })
-	  .style("stroke", function(d) { return color(d.data.value); } )
-	  .style("fill", "none" );
-    
+          .style("stroke", function(d) { return color(d.data.value); } )
+          .style("fill", "none" );
+
       var node = g.selectAll(".node")
           .data(root.descendants())
-        .enter().append("g")
+          .enter().append("g")
           .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
           .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; });
-	  //.style("fill", function(d) { return color(d.data.value); } );
-    
+          //.style("fill", function(d) { return color(d.data.value); } );
+
       node.append("circle")
           .attr("r", 2.5)
-	  .style("fill", function(d) { return color(d.data.value); } );
-    
+          .style("fill", function(d) { return color(d.data.value); } );
+
       node.append("text")
           .attr("dy", "0.31em")
           .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
           .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
           .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
           .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); })
-	  .style("opacity", 0);
+          .style("opacity", 0);
     });
-    
+
     function project(x, y) {
       var angle = (x - 90) / 180 * Math.PI, radius = y;
       return [radius * Math.cos(angle), radius * Math.sin(angle)];
     }
-  </script>
-</body>
+}
