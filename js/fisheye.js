67592d3ad17069f034cabc379e72748e172b2e34
Donatien Delehelle <ddelehel@gmail.com>
2020-02-17 21:21:23 +0100

    deshysterisation du warp

--- js/fisheye.js
+++ js/fisheye.js
@@ -45,6 +45,56 @@
       };
 
       return rescale();
+    },
+    closedRadial: function() {
+      var radius = 200,
+          distortion = 2,
+          frontier = 100,
+          k0,
+          k1,
+          focus = [0, 0];
+
+      function fisheye(d) {
+        var dx = d.x - focus[0],
+            dy = d.y - focus[1],
+            dd = Math.sqrt(dx * dx + dy * dy);
+        if (!dd || dd >= radius) return {x: d.x, y: d.y, z: dd >= radius ? 1 : 10};
+        var k = k0 * (1 - Math.exp(-dd * k1 * (1 -  (d.y / frontier - 1)*(d.y / frontier - 1)*(d.y / frontier - 1)*(d.y / frontier - 1)))) / dd * .75 + .25;
+        return {x: focus[0] + dx * k, y: focus[1] + dy * k, z: Math.min(k, 10)};
+      }
+
+      function rescale() {
+        k0 = Math.exp(distortion);
+        k0 = k0 / (k0 - 1) * radius;
+        k1 = distortion / radius;
+        return fisheye;
+      }
+
+      fisheye.radius = function(_) {
+        if (!arguments.length) return radius;
+        radius = +_;
+        return rescale();
+      };
+
+      fisheye.frontier = function(_) {
+        if (!arguments.length) return frontier;
+        frontier = +_;
+        return rescale();
+      };
+
+      fisheye.distortion = function(_) {
+        if (!arguments.length) return distortion;
+        distortion = +_;
+        return rescale();
+      };
+
+      fisheye.focus = function(_) {
+        if (!arguments.length) return focus;
+        focus = _;
+        return fisheye;
+      };
+
+      return rescale();
     }
   };
 
