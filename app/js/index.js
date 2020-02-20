43b174923b5dc0fa77bf0f8747f20fdd47a5732c
AntoTr4 <antonin.leyval@ecl16.ec-lyon.fr>
2020-02-20 20:01:39 +0100

    Implementing about.html with dynamic link to index

--- app/js/index.js
+++ app/js/index.js
@@ -12,4 +12,17 @@ jQuery($ => {
 			location.reload();
 		}
 	);
+
+	$('#animal_from_about').click(() => 
+		{
+			localStorage.setItem("current-reign-url", "https://raw.githubusercontent.com/ddonatien/endspe/master/app/data/phylo_animal_genus.csv");
+			window.location.assign("index.html");
+		}
+	);
+	$('#plant_from_about').click(() => 
+		{
+			localStorage.setItem("current-reign-url", "https://raw.githubusercontent.com/ddonatien/endspe/master/app/data/phylo_plant_genus.csv");
+			window.location.assign("index.html");
+		}
+	);
 })
