cf1a1ca3cc72fc44731c981d320644dc5040445d
AntoTr4 <antonin.leyval@ecl16.ec-lyon.fr>
2020-02-27 14:38:04 +0100

    Test loading of csv datas

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,58 @@
+// var phyloItems = JSON.parse('{{itemList | tojson | safe}}');
+
+// var items = [];
+// phyloItems.forEach(item => items.push(item))
+
+// new Autocomplete('#autocomplete', {
+//   search: input => {
+//     if (input.length < 1) { return [] }
+//     var items1 = items.filter(title => {
+//       return title.toLowerCase()
+//         .startsWith(input.toLowerCase())
+//     });
+//     var items2 = [];
+//     if (input.length >= 3){
+//         items2 = items.filter(title => {
+//           return (
+//             title.toLowerCase()
+//             .includes(input.toLowerCase())) &&
+//             (!(title.toLowerCase()
+//             .startsWith(input.toLowerCase())))
+//         });
+//     }
+//     return items1.concat(items2)
+//   },
+//   onSubmit: result => {
+//     if ( itemList.indexOf(result) >= 0 ) {
+//         window.location.assign('item/' + `${result}`);
+//     }
+//     else if ( authorList.indexOf(result) >= 0 ) {
+//         window.location.assign('author/' + `${result}`);
+//     }
+//   }
+// })
+
+	
+function loadCSV(url) {
+	var data;
+	
+	$.ajax({
+	  type: "GET",  
+	  url: url,
+	  dataType: "text",       
+	  success: function(response)  
+	  {
+		data = $.csv.toArrays(response);
+	  }   
+	});
+
+	return data
+}
+
 jQuery($ => {
+
+	var phyloDatas = loadCSV("data/phylo.csv");
+	console.log(phyloDatas);
 
 	$('.display_animal').click(() => 
 		{
